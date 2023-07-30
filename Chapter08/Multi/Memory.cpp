ade2cc9680999276a8e00b7fbd7921928615e246
M3noetius <gdtsek@gmail.com>
2023-07-30 09:12:14 +0300

    Everything in one solution. Can not only compile 12-BackUp

--- Chapter08/Multi/Memory.cpp
+++ Chapter08/Multi/Memory.cpp
@@ -2,7 +2,7 @@
 #include "Memory.h"
 
 void* __cdecl operator new(size_t size, POOL_TYPE pool, ULONG tag) {
-	return ExAllocatePoolWithTag(pool, size, tag);
+	return ExAllocatePool2(pool, size, tag);
 }
 
 void __cdecl operator delete(void* p, size_t) {
